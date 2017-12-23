a4b460956a4744aeb5ac4284119feaa00d221c8b
Juan Valencia <juan7732@gmail.com>
2017-12-22 20:28:55 -0600

    finished day 10 part 1, part 2 is incredibly complex

---
+++ day 10/1day10.py
@@ -0,0 +1,33 @@
+# Circular Hash
+
+def flip_list(length, currentPosition):
+    reverseList = []
+    for j in range(currentPosition, currentPosition + length):
+        if j >= len(a):
+            reverseList.append(a[j % len(a)])
+        else:
+            reverseList.append(a[j])
+    o = 0
+    for k in range(currentPosition, currentPosition + length):
+        if k >= len(a):
+            a[k % len(a)] = reverseList[len(reverseList) - 1 - o]
+            o += 1
+        else:
+            a[k] = reverseList[len(reverseList) - 1 - o]
+            o += 1
+    print(a)
+
+
+a = list(range(0, 256))
+currentPositionOut = 0
+skipSize = 0
+
+pattern = [88, 88, 211, 106, 141, 1, 78, 254, 2, 111, 77, 255, 90, 0, 54, 205]
+
+
+for l in range(0, len(pattern)):
+    flip_list(pattern[l], currentPositionOut)
+    currentPositionOut = (currentPositionOut + pattern[l] + skipSize) % len(a)
+    skipSize += 1
+
+print(str(a[0] * a[1]))
